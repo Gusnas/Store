b1da22d8513c019436913614f94617b7fdbf2eea
Gustavo Nascimento <gusnas@alu.ufc.br>
2021-07-22 16:04:06 -0300

    First commit with the basic structure

---
+++ Store/Entities/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; } 
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment)
+        {
+            Moment = moment;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            
+        }
+        
+        public void RemoveItem(OrderItem item)
+        {
+
+        }
+
+        public double Total()
+        {
+            
+        }
+    }
+}
---
+++ Store/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Store.Entities;
+using Store.Entities.Enums;
+
+namespace Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
